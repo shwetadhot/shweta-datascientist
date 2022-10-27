095b4143604f53accdaecc9de32a9c56d082b15f
shwetadhot <116790956+shwetadhot@users.noreply.github.com>
2022-10-27 12:52:21 +0530

    Add files via upload

---
+++ AD.py
@@ -0,0 +1,129 @@
+print ("1st Oct 2022")
+
+'''
+a=int(input("Enter a Number : "))
+if a==5:                             # Condition check
+    print("It is Correct")
+else:                               # When condition is false
+    print("It is not Correct")
+
+'''
+
+
+#Greater of Two Number
+'''
+x=float(input("Enter 1st Number : "))
+y=float(input("Enter 2nd Number : "))
+
+if x>y:
+    print(x," is Greater")
+else:
+    print(y," is Greater")
+'''
+
+#Even & Odd Number
+'''
+a=float(input("Enter any Number : "))
+
+if a%2==0:
+    print(a," is Even Number")
+else:
+    print(a," is Odd Number")
+'''
+
+#Greater of Three Number
+'''
+x=float(input("Enter 1st Number : "))
+y=float(input("Enter 2nd Number : "))
+z=float(input("Enter 3rd Number : "))
+
+if x>y and x>z:
+    print(x," is Greater")
+elif y>x and y>z:
+        print(y," is Greater")
+else:
+    print(z," is Greater")
+'''
+
+# Time
+'''
+h=float(input("Enter Hours : "))
+
+if h<12:
+    print("Good Morning")
+elif h>=12 and h<17:
+    print("Good Afternoon")
+elif h>=17 and h<19:
+    print("Good Evening")
+elif h>19 and h<=24:
+    print("Good Night")
+else:
+    print("You enter wrong time...")
+'''
+
+# Areaof Traingle
+'''
+h=float(input("Enter the height of Traingle : "))
+b=float(input("Enter the base of Traingle : "))
+
+area_Trigle=0.5*h*b
+
+print("Area of Traingle is : ",area_Trigle)
+
+
+#Area of sphere
+
+r=float(input("Enter the Radius : "))
+
+volume_sphere=4/3*3.14*r*r*r
+
+print("Volume of Sphere is : ",volume_sphere)
+'''
+
+#Number is Negative, Positive or Zero
+'''
+x=float(input("Enter any Number : "))
+
+if x>0:
+    print(x," is positive")
+elif x<0:
+    print(x," is negative")
+else:
+    print(x,"You Entered Zero")
+'''
+
+#Leap Year
+
+'''year=int(input("Enter Year : "))
+
+if year%4==0 and year%100!=0:
+    print("The Year",year,"is Leap Year")
+else:
+    print("The Year",year,"is not Leap Year")
+'''
+
+'''
+if year%400==0:
+    print("leap year")
+else:
+    print("The Year",year,"is not Leap Year")
+'''
+
+Abhijeet=True
+if Abhijeet:
+     print("Hi")
+else:
+    print("bye")
+
+if 9>=9:
+    print("hello")
+else:
+    print("Bye")
+if hi: # Error
+    print("hi")
+else:
+    print("bye")
+
+if  
+    print("o")
+    
